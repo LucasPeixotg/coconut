26343280517c943d170da4dc5df43abcb965c62c
peixoto <peixoto@pop-os.localdomain>
2024-02-01 10:30:46 -0300

    initial help screen added

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
 	// new program
-	p := tea.NewProgram(NewModel(),
+	p := tea.NewProgram(newModel(),
 		tea.WithAltScreen(), // fullscreen
 	)
 
@@ -21,25 +24,88 @@ func main() {
 	}
 }
 
-// main model: indicates the current state
-type Model struct{}
+// keybindings
+type keyMap struct {
+	Quit  key.Binding
+	Help  key.Binding
+	OpenF key.Binding
+	OpenD key.Binding
+	New   key.Binding
+}
 
-func NewModel() *Model {
-	return &Model{}
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{{k.New, k.OpenF, k.OpenD}, {k.Help, k.Quit}}
+}
+
+// main model: indicates the current state
+type model struct {
+	keys     keyMap
+	help     help.Model
+	quitting bool
+	width    int
+	height   int
+}
+
+func newModel() *model {
+	keys := keyMap{
+		Quit: key.NewBinding(
+			key.WithKeys("esc", "ctrl+c"),
+			key.WithHelp("esc", "quit"),
+		),
+		Help: key.NewBinding(
+			key.WithKeys("?", "h"),
+			key.WithHelp("?", "toggle help"),
+		),
+		New: key.NewBinding(
+			key.WithKeys("ctrl+n"),
+			key.WithHelp("crtl+n", "new file"),
+		),
+		OpenF: key.NewBinding(
+			key.WithKeys("ctrl+o"),
+			key.WithHelp("ctrl+o", "open file"),
+		),
+		OpenD: key.NewBinding(
+			key.WithKeys("ctrl+d"),
+			key.WithHelp("ctrl+d", "open dir"),
+		),
+	}
+
+	return &model{
+		help: help.New(),
+		keys: keys,
+	}
+}
+
+// set correct window size on all relevant components
+func (m *model) setSizing(width, height int) {
+	m.help.Width = width
+
+	m.width = width
+	m.height = height
 }
 
 // initializes the event loop
-func (m Model) Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	return nil
 }
 
 // updates the model based on received messages
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.setSizing(msg.Width, msg.Height)
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc":
-			// closes app on [esc] press
+		switch {
+		case key.Matches(msg, m.keys.Help):
+			// toggle full help
+			m.help.ShowAll = !m.help.ShowAll
+		case key.Matches(msg, m.keys.Quit):
+			// closes app
+			m.quitting = true
 			return m, tea.Quit
 		}
 	}
@@ -48,6 +114,13 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 // renders UI string, based on the current state
-func (m Model) View() string {
-	return "test"
+func (m model) View() string {
+	if m.quitting {
+		return "See you :)"
+	}
+
+	// centralize help
+	s := lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, m.help.View(m.keys))
+
+	return s
 }
